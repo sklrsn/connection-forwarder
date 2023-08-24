1666631e01330b345b4f518117a99241c01fe418
Kalai <kalaiarasan.saminathan@ssh.com>
2023-08-24 16:00:15 +0300

    fix connection exporter

---
+++ connection-exporter/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "=> starting connection exporter"
+/opt/exporter/connection-exporter
--- connection-exporter/ffmpeg-export.go
+++ connection-exporter/ffmpeg-export.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 )
 
 func init() {
@@ -9,5 +10,43 @@ func init() {
 }
 
 func main() {
-	log.Println("starting exporter")
+	log.Println("starting exporter at 9900")
+
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.Handle("/list", http.FileServer(http.Dir("/opt/storage")))
+
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			storageID := r.URL.Query().Get("storage_id")
+
+			log.Printf("Received storage id %v for encode", storageID)
+			w.WriteHeader(http.StatusOK)
+
+			return
+		} else {
+			http.Error(w, "unsupported http method", http.StatusBadRequest)
+			return
+		}
+	})
+
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			videoID := r.URL.Query().Get("video_id")
+
+			log.Printf("Received download %v for download", videoID)
+			w.WriteHeader(http.StatusOK)
+
+			return
+		} else {
+			http.Error(w, "unsupported http method", http.StatusBadRequest)
+			return
+		}
+	})
+
+	log.Fatalf("%v", http.ListenAndServe(":9900", nil))
+
 }
